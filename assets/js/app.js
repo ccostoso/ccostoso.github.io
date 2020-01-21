a4915d05ad86f145dabd919fdbb5105820c70803
ccostoso <carlos.costoso@outlook.com>
2020-01-21 12:57:55 -0500

    Removed border from .card elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,39 @@
-const pageArr = ["about", "portfolio", "contact"];
-let currentPage = pageArr[0];
-let lastPage;
+$(document).ready(function() {
+    const pageArr = ["about", "portfolio", "contact"];
+    let currentPage = pageArr[0];
+    let lastPage;
 
-function proceed(lastPage, currentPage) {
-    let direction;
+    function proceed(lastPage, currentPage) {
+        let direction;
 
-    if (pageArr.indexOf(lastPage) > pageArr.indexOf(currentPage)) {
-        direction = "right";
-    } else {
-        direction = "left";
+        if (pageArr.indexOf(lastPage) > pageArr.indexOf(currentPage)) {
+            direction = "right";
+        } else {
+            direction = "left";
+        }
+
+        if (lastPage !== currentPage) {
+            $(`#${lastPage}`).removeClass("in-fade");
+            $(`#${lastPage}`).addClass(`${direction}-fade`);
+
+            setTimeout(function() {
+                $(`#${lastPage}`).addClass("invisible");
+            }, 350)
+        
+            setTimeout(function() {
+                $(`#${lastPage}`).toggleClass("invisible d-none");
+                $(`#${lastPage}`).removeClass(`${direction}-fade`);
+        
+                $(`#${currentPage}`).removeClass(`${direction}-fade d-none`);
+                $(`#${currentPage}`).addClass("in-fade");
+            }, 400);
+        }
     }
 
-    if (lastPage !== currentPage) {
-        $(`#${lastPage}`).removeClass("in-fade");
-        $(`#${lastPage}`).addClass(`${direction}-fade`);
-
-        setTimeout(function() {
-            $(`#${lastPage}`).addClass("invisible");
-        }, 350)
-    
-        setTimeout(function() {
-            $(`#${lastPage}`).toggleClass("invisible d-none");
-            $(`#${lastPage}`).removeClass(`${direction}-fade`);
-    
-            $(`#${currentPage}`).removeClass(`${direction}-fade d-none`);
-            $(`#${currentPage}`).addClass("in-fade");
-        }, 400);
-    }
-}
-
-$(".window-link").on("click", function() {
-    event.preventDefault();
-    lastPage = currentPage;
-    currentPage = $(this).data("name");
-    proceed(lastPage, currentPage);
+    $(".window-link").on("click", function() {
+        event.preventDefault();
+        lastPage = currentPage;
+        currentPage = $(this).data("name");
+        proceed(lastPage, currentPage);
+    });
 });
