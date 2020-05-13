594c67259b6e692f5b7d195bb810a30133c22d9a
ccostoso <carlos.costoso@outlook.com>
2020-05-12 21:51:09 -0400

    Updated portfolio with links to deployed projects

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,28 +4,28 @@ export function projectPage() {
             name: "rpg",
             formattedName: "RPG",
             github: "Homework4",
-            description: "This Dragon Quest themed arena fighter uses monsters and sprites from 2001's Dragon Warrior VII. Technologies used include Bootstrap and jQuery.",
+            description: "<p>This Dragon Quest themed arena fighter uses monsters and sprites from 2001's Dragon Warrior VII. Technologies used include Bootstrap and jQuery.</p><p>You can find the deployed project <a href=\"https://ccostoso.github.io/Homework4/\">here</a>.</p>",
             active: true
         },
         {
             name: "trivia",
             formattedName: "Trivia",
             github: "Homework5",
-            description: "This basic quiz game employs timeouts, counters and GIFs. Technologies used include Bootstrap and jQuery.",
+            description: "<p>This basic quiz game employs timeouts, counters and GIFs. Technologies used include Bootstrap and jQuery.</p><p>You can find the deployed project <a href=\"https://ccostoso.github.io/Homework5/\">here</a>.</p>",
             active: true
         },
         {
             name: "nyt",
             formattedName: "NYT Scraper",
             github: "NYT-Scraper",
-            description: "This app uses a MongoDB database to store article headlines scraped with the cheerio Node package module from the New York Times front page.",
+            description: "<p>This app uses a MongoDB database to store article headlines scraped with the cheerio Node package module from the New York Times front page.</p><p>You can find the deployed project <a href=\"https://cjc-nyt-scraper.herokuapp.com/\">here</a>.</p>",
             active: false
         }, 
         {
             name: "mutuaid",
             formattedName: "MutuAid",
             github: "Covid-19-Mutual-Aid",
-            description: "I led a team to develop a Reddit-like application to connect people trying to build local mutual aid networks. Technologies used include MongoDB/Mongoose, Express, React, Node, Passport and react-intl.",
+            description: "<p>I led a team to develop a Reddit-like application to connect people trying to build local mutual aid networks. Technologies used include MongoDB/Mongoose, Express, React, Node, Passport and react-intl.</p><p>You can find the deployed project <a href=\"https://mutuaid.herokuapp.com/\">here</a>.</p>",
             active: false
         }
     ];
@@ -34,7 +34,7 @@ export function projectPage() {
         console.log(ele);
         let projectFig = $("<figure>");
         projectFig.mouseover(function() {
-            $("#project-desc").text(ele.description);
+            $("#project-desc").html(ele.description);
         })
         projectFig.addClass("card col-xs col-md-4 p-0 mr-4 mb-4 overflow-hidden");
         let projectAnchor;
